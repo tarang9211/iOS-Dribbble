feb5e4cfa52773df31842688d82c71975f1cd416
Tarang Hirani <tarang9211@gmail.com>
2016-12-28 02:49:58 +0530

    removed table view controller and implemented a collection view controller instead

--- Dribble/ViewController.swift
+++ Dribble/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ShotsViewController: UITableViewController {
+class ShotsViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

035bfd49a8a501c71e2358027ef826bbf94a5622
Tarang Hirani <tarang9211@gmail.com>
2017-01-04 01:54:32 +0530

    added basic unit test to get started

--- DribbleTests/DribbleTests.swift
+++ DribbleTests/DribbleTests.swift
@@ -21,16 +21,16 @@ class DribbleTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testURLIsAppropriate() {
+        let url = Config.rootUrl + "?access_token=\(Config.accessToken)"
+        XCTAssertNotNil(url, "URL is not nil")
     }
     
 }
